201f265406f573fecf52049542321b7d3f98930f
18037689 <Zh3nn4n>
2020-08-06 12:10:30 +0800

    Initial commit

---
+++ app/src/main/java/com/example/demonavigation/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.demonavigation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnNewActivity;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnNewActivity = findViewById(R.id.btnNewActivity);
+
+        btnNewActivity.setOnClickListener(new View.OnClickListener() {
+                  @Override
+                  public void onClick(View v) {
+                      Intent i = new Intent(MainActivity.this,
+                              NewActivity.class);
+                      startActivity(i);
+                  }
+              });
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //action
+                Toast.makeText(MainActivity.this, "Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
